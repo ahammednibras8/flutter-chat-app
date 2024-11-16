7f70b4dfb3909a9318117b763ecbe94f642636e1
ahammednibras8 <ahammednibras@proton.me>
2024-11-16 15:29:23 +0530

    Added Phone Text Field

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -6,5 +6,6 @@ class AppColors {
 
   //Neutral
   static const Color active = Color(0xff0F1828);
+  static const Color disabled = Color(0xffADB5BD);
   static const Color offWhite = Color(0xffF7F7FC);
 }
--- lib/screens/phone_login_screen.dart
+++ lib/screens/phone_login_screen.dart
@@ -1,31 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/core/app_colors.dart';
+import 'package:flutter_chat_app/core/app_typography.dart';
 import 'package:flutter_chat_app/widgets/login_info_text.dart';
 import 'package:flutter_chat_app/widgets/my_appbar.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
 
 class PhoneLoginScreen extends StatelessWidget {
   const PhoneLoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: MyAppbar(
-        leadingIcon: Icon(Icons.arrow_back_ios_new_rounded),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 32),
-              child: LoginInfoText(
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: const Scaffold(
+        appBar: MyAppbar(
+          leadingIcon: Icon(Icons.arrow_back_ios_new_rounded),
+        ),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 32),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              LoginInfoText(
                 title: "Enter Your Phone Number",
                 subtitle:
                     "Please confirm your country code and enter your phone number",
               ),
-            ),
-          ],
+              SizedBox(height: 48),
+              PhoneInputText()
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class PhoneInputText extends StatelessWidget {
+  const PhoneInputText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IntlPhoneField(
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: AppColors.offWhite,
+        labelText: 'Phone Number',
+        counterText: '',
+        floatingLabelBehavior: FloatingLabelBehavior.never,
+        labelStyle: AppTypography.body1.copyWith(
+          color: AppColors.disabled,
+        ),
+        enabledBorder: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: AppColors.defaultColor,
+          ),
+        ),
+        border: OutlineInputBorder(
+          borderSide: const BorderSide(
+            color: AppColors.defaultColor,
+          ),
+          borderRadius: BorderRadius.circular(4),
+        ),
+        focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: AppColors.defaultColor,
+            width: 1.8,
+          ),
+        ),
+      ),
+      initialCountryCode: 'IN',
     );
   }
 }
