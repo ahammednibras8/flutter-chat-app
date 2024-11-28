af188c9fc9bc443a8911dcd8d291c3075598ab4b
ahammednibras8 <ahammednibras@proton.me>
2024-11-28 12:15:07 +0530

    Updated Profile Pic selector

--- lib/widgets/profile_pic_selector.dart
+++ lib/widgets/profile_pic_selector.dart
@@ -6,24 +6,36 @@ import 'package:flutter_chat_app/core/app_typography.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ProfilePicSelector extends StatefulWidget {
-  const ProfilePicSelector({super.key});
+  final String? currentImageUrl;
+  final Function(File)? onImageSelected;
+  const ProfilePicSelector({
+    super.key,
+    this.currentImageUrl,
+    this.onImageSelected,
+  });
 
   @override
   State<ProfilePicSelector> createState() => _ProfilePicSelectorState();
 }
 
 class _ProfilePicSelectorState extends State<ProfilePicSelector> {
-  File? _image;
+  File? _selectedImage;
   final ImagePicker _picker = ImagePicker();
 
   Future<void> _pickImage(ImageSource source) async {
     try {
-      final XFile? pickedFile = await _picker.pickImage(source: source);
+      final XFile? pickedFile = await _picker.pickImage(
+        source: source,
+        imageQuality: 70,
+        maxWidth: 512,
+        maxHeight: 512,
+      );
 
       if (pickedFile != null) {
         setState(() {
-          _image = File(pickedFile.path);
+          _selectedImage = File(pickedFile.path);
         });
+        widget.onImageSelected?.call(_selectedImage!);
       }
     } catch (e) {
       debugPrint('Error picking image: $e');
@@ -33,38 +45,39 @@ class _ProfilePicSelectorState extends State<ProfilePicSelector> {
   void _showImageSourceDialog() {
     showModalBottomSheet(
       context: context,
-      builder: (BuildContext context) {
-        return Container(
-          height: 150,
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              ListTile(
-                leading: const Icon(Icons.camera_alt),
-                title: const Text(
-                  'Take a photo',
-                  style: AppTypography.body1,
-                ),
-                onTap: () {
-                  Navigator.pop(context);
-                  _pickImage(ImageSource.camera);
-                },
+      backgroundColor: AppColors.offWhite,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      builder: (context) => SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              leading: const Icon(
+                Icons.photo_camera_outlined,
+                color: AppColors.defaultColor,
               ),
-              ListTile(
-                leading: const Icon(Icons.photo_library),
-                title: const Text(
-                  'Choose from gallery',
-                  style: AppTypography.body1,
-                ),
-                onTap: () {
-                  Navigator.pop(context);
-                  _pickImage(ImageSource.gallery);
-                },
+              title: const Text('Take a Photo'),
+              onTap: () {
+                Navigator.pop(context);
+                _pickImage(ImageSource.camera);
+              },
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.photo_library_outlined,
+                color: AppColors.defaultColor,
               ),
-            ],
-          ),
-        );
-      },
+              title: const Text('Choose from Gallery'),
+              onTap: () {
+                Navigator.pop(context);
+                _pickImage(ImageSource.gallery);
+              },
+            )
+          ],
+        ),
+      ),
     );
   }
 
@@ -74,26 +87,51 @@ class _ProfilePicSelectorState extends State<ProfilePicSelector> {
       children: [
         GestureDetector(
           onTap: _showImageSourceDialog,
-          child: Container(
-            width: 120,
-            height: 120,
-            decoration: BoxDecoration(
-              color: AppColors.offWhite,
-              shape: BoxShape.circle,
-              image: _image != null
-                  ? DecorationImage(
-                      image: FileImage(_image!),
-                      fit: BoxFit.cover,
-                    )
-                  : null,
-            ),
-            child: _image == null
-                ? const Icon(
-                    Icons.add_photo_alternate_rounded,
-                    size: 40,
+          child: Stack(
+            children: [
+              Container(
+                width: 140,
+                height: 140,
+                decoration: BoxDecoration(
+                  color: AppColors.disabled.withOpacity(0.1),
+                  shape: BoxShape.circle,
+                  image: _selectedImage != null
+                      ? DecorationImage(
+                          image: FileImage(_selectedImage!),
+                          fit: BoxFit.cover,
+                        )
+                      : widget.currentImageUrl != null
+                          ? DecorationImage(
+                              image: NetworkImage(widget.currentImageUrl!),
+                              fit: BoxFit.cover,
+                            )
+                          : null,
+                ),
+                child: _selectedImage == null && widget.currentImageUrl == null
+                    ? const Icon(
+                        Icons.person,
+                        size: 80,
+                        color: AppColors.active,
+                      )
+                    : null,
+              ),
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: const BoxDecoration(
+                    color: AppColors.offWhite,
+                    shape: BoxShape.circle,
+                  ),
+                  child: const Icon(
+                    Icons.camera_alt_outlined,
                     color: AppColors.active,
-                  )
-                : null,
+                    size: 20,
+                  ),
+                ),
+              )
+            ],
           ),
         ),
       ],
